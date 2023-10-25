5d44fa4e969f2e6e1cc0a3e7459d4df7f634b403
wmfsdev <117413083+wmfsdev@users.noreply.github.com>
2023-10-25 19:45:46 +0100

    add img srcset for desktop and mobile. add about me

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 
-const menuIcon = document.querySelector('nav')
+const menuIcon = document.querySelector('.menu')
 const list = document.querySelector('nav ul li')
 list.classList.add('not-active')
 
 menuIcon.addEventListener("mouseenter", () => {
-  
+//  menuIcon.style.animation = "shake 0.8s cubic-bezier(0.455, 0.030, 0.515, 0.955) both"
   setTimeout(() => {
     list.classList.remove('not-active')
   }, 300);
 })
 
-// menuIcon.addEventListener("mouseenter", (e) => {
-//   setTimeout(() => {
-//   }, 500);
+menuIcon.addEventListener("touchstart", () => {
+  //  menuIcon.style.animation = "shake 0.8s cubic-bezier(0.455, 0.030, 0.515, 0.955) both"
+    setTimeout(() => {
+      list.classList.remove('not-active')
+    }, 300);
+  })
 
-//   console.log("open")
-//   menuIcon.style.animation = "open-menu 0.35s ease-in forwards"
-// })
 
 // const menu = document.querySelector('nav ul')
 // menu.addEventListener("mouseleave", () => {
@@ -28,4 +28,9 @@ menuIcon.addEventListener("mouseenter", () => {
 // header.addEventListener("mouseover", () => {
 //   console.log("header")
 //   menuIcon.style.animation = "close-menu 0.5s ease-in forwards"
+// })
+
+
+// document.addEventListener("scroll", (e) => {
+//   console.log(e)
 // })
