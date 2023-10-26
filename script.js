4c6699bdc0547d8c158a73c4e7f06fb9f04a54ce
wmfsdev <117413083+wmfsdev@users.noreply.github.com>
2023-10-26 18:27:54 +0100

    change 'about-me' layout and aesthetic

--- script.js
+++ script.js
@@ -15,7 +15,18 @@ menuIcon.addEventListener("touchstart", () => {
     setTimeout(() => {
       list.classList.remove('not-active')
     }, 300);
-  })
+})
+
+const about = document.querySelector('.animate-me')
+about.addEventListener("click", () => {
+  
+  const firstPara = document.querySelector('.first-para')
+  const secondPara = document.querySelector('.second-para')
+  firstPara.classList.remove('first-para')
+  secondPara.classList.remove('second-para')
+  firstPara.classList.add('first-para-visible')
+  secondPara.classList.add('second-para-visible')
+})
 
 
 // const menu = document.querySelector('nav ul')
